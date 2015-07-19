9eb8720ffbf49d94312e692674f100c473977b7e
Brian Mearns <bmearns@ieee.org>
2015-07-18 22:58:42 -0400

    Finished upgrading groovy generator to use indented writer.

--- src/main/groovy/org/ieee/bmearns/metabuilder/generators/GroovyGenerator.groovy
+++ src/main/groovy/org/ieee/bmearns/metabuilder/generators/GroovyGenerator.groovy
@@ -21,79 +21,6 @@ class GroovyGenerator extends Generator {
         this(new IndentedWriter.Factory("    ", 0))
     }
 
-    protected void writeBuilderMethods(IndentedWriter writer, Buildable buildable, Buildable.Property prop) {
-
-        if(prop.array) {
-
-            String upperCasePropName = prop.name.replaceFirst('.') { it.toUpperCase() }
-
-            writer
-                .writeLines(
-                    "/**",
-                    " * Builder method to clear out all elements currently specified for",
-                    " * the {@link ${buildable.name}#${prop.name}} property.",
-                    " */",
-                    "public ${buildable.builderName} clear${upperCasePropName}() {"
-                )
-                .block(1).writeLines(
-                        "this.${prop.name}.clear();",
-                        "return this;"
-                )
-                .endBlock(1).writeLine(
-                    "}"
-                )
-
-            writer
-                .writeLines(
-                    "/**",
-                    " * Builder methods to add any number of elements for the",
-                    " * {@link ${buildable.name}#${prop.name}} property.",
-                    " */",
-                    "public ${buildable.builderName} ${prop.name}(${prop.type.name}... ${prop.name}) {"
-                )
-                .block(1).writeLines(
-                        "this.${prop.name}.addAll(Arrays.<${prop.type.name}>asList(${prop.name}));",
-                        "return this;"
-                )
-                .endBlock(1).writeLine(
-                    "}",
-                )
-            
-            writer
-                .writeLines(
-                    "/**",
-                    " * Builder methods to add any number of elements for the",
-                    " * {@link ${buildable.name}#${prop.name}} property.",
-                    " */",
-                    "public ${buildable.builderName} ${prop.name}(List<${prop.type.name}> ${prop.name}) {"
-                )
-                .block(1).writeLines(
-                    "this.${prop.name}.addAll(${prop.name});",
-                    "return this;"
-                )
-                .endBlock(1).writeLine(
-                    "}"
-                )
-            
-        }
-        else {
-            writer
-                .writeLines(
-                    "/**",
-                    " * Builder methods to set the value of the",
-                    " * {@link ${buildable.name}#${prop.name}} property.",
-                    " */",
-                    "public ${buildable.builderName} ${prop.name}(${prop.type.name} ${prop.name}) {"
-                )
-                .block(1).writeLines(
-                    "this.${prop.name} = ${prop.name};",
-                    "return this;"
-                )
-                .endBlock(1).writeLine(
-                    "}"
-                )
-        }
-    }
 
     protected void writeImports(IndentedWriter writer, Buildable buildable) {
         ArrayList<String> imports = new ArrayList<>(buildable.collectImports{ it })
@@ -110,7 +37,6 @@ class GroovyGenerator extends Generator {
     }
 
     protected void writePropFields(IndentedWriter writer, Buildable buildable) {
-
         buildable.props.each {
             String comment = it.comment
             String tail = ""
@@ -136,6 +62,148 @@ class GroovyGenerator extends Generator {
         }
     }
 
+    protected void writeConstructor(IndentedWriter writer, Buildable buildable) {
+        writer.writeStart("public ${buildable.name}(")
+        writer.write(
+            buildable.props.collect{ "${it.type.name} ${it.name}" }.join(', ')
+        )
+        writer.endLine(") {")
+        writer.block(1)
+        buildable.props.each {
+            writer.writeLine("this.${it.name} = ${it.name};")
+        }
+        writer.endBlock(1)
+        writer.writeLine("}")
+    }
+
+    protected void writeBuilderFields(IndentedWriter writer, Buildable buildable) {
+        buildable.props.each {
+            if(it.array) {
+                writer.writeStart("List<${it.type.name}>")
+            }
+            else {
+                writer.writeStart("${it.type.name}")
+            }
+            writer.endLine(" ${it.name};")
+        }
+    }
+
+    protected void writeBuilderInitBlocks(IndentedWriter writer, Buildable buildable) {
+        if (buildable.props.any{ it.array }) {
+            writer
+                .writeLine("{")
+                .block(1)
+
+            buildable.props.each {
+                if(it.array) {
+                    writer.writeLine("${it.name} = new LinkedList<${it.type.name}>();")
+                }
+            }
+                    
+            writer
+                .endBlock(1)
+                .writeLine("}")
+                .skip()
+        }
+    }
+
+    protected void writePropBuilderMethods(IndentedWriter writer, Buildable buildable, Buildable.Property prop) {
+
+        if(prop.array) {
+
+            String upperCasePropName = prop.name.replaceFirst('.') { it.toUpperCase() }
+
+            writer
+                .writeLines(
+                    "/**",
+                    " * Builder method to clear out all elements currently specified for",
+                    " * the {@link ${buildable.name}#${prop.name}} property.",
+                    " */",
+                    "public ${buildable.builderName} clear${upperCasePropName}() {"
+                )
+                .block(1).writeLines(
+                        "this.${prop.name}.clear();",
+                        "return this;"
+                )
+                .endBlock(1).writeLine(
+                    "}"
+                )
+                .skip()
+
+            writer
+                .writeLines(
+                    "/**",
+                    " * Builder methods to add any number of elements for the",
+                    " * {@link ${buildable.name}#${prop.name}} property.",
+                    " */",
+                    "public ${buildable.builderName} ${prop.name}(${prop.type.name}... ${prop.name}) {"
+                )
+                .block(1).writeLines(
+                        "this.${prop.name}.addAll(Arrays.<${prop.type.name}>asList(${prop.name}));",
+                        "return this;"
+                )
+                .endBlock(1).writeLine(
+                    "}",
+                )
+                .skip()
+            
+            writer
+                .writeLines(
+                    "/**",
+                    " * Builder methods to add the given list of elements for the",
+                    " * {@link ${buildable.name}#${prop.name}} property.",
+                    " */",
+                    "public ${buildable.builderName} ${prop.name}(List<${prop.type.name}> ${prop.name}) {"
+                )
+                .block(1).writeLines(
+                    "this.${prop.name}.addAll(${prop.name});",
+                    "return this;"
+                )
+                .endBlock(1).writeLine(
+                    "}"
+                )
+                .skip()
+        }
+        else {
+            writer
+                .writeLines(
+                    "/**",
+                    " * Builder methods to set the value of the",
+                    " * {@link ${buildable.name}#${prop.name}} property.",
+                    " */",
+                    "public ${buildable.builderName} ${prop.name}(${prop.type.name} ${prop.name}) {"
+                )
+                .block(1).writeLines(
+                    "this.${prop.name} = ${prop.name};",
+                    "return this;"
+                )
+                .endBlock(1).writeLine(
+                    "}"
+                )
+                .skip()
+        }
+    }
+
+    protected void writeBuilderMethods(IndentedWriter writer, Buildable buildable) {
+        buildable.props.each {
+            writePropBuilderMethods(writer, buildable, it)
+        }
+    }
+
+    protected void writeBuilderClass(IndentedWriter writer, Buildable buildable) {
+        //TODO: Use an AbstractBuilder class.
+        writer.writeLine("public static class ${buildable.builderName} implements Builder<${buildable.name}> {")
+        writer.block(1)
+
+        writeBuilderFields(writer, buildable)
+        writer.skip()
+        writeBuilderInitBlocks(writer, buildable)
+        writeBuilderMethods(writer, buildable)
+
+        writer.endBlock(1)
+        writer.writeLine("}")
+    }
+
     @Override
     public void generate(Buildable buildable, Writer writer) {
         IndentedWriter iwriter = writerFactory.build(writer)
@@ -153,8 +221,15 @@ class GroovyGenerator extends Generator {
         )
         iwriter.block(1)
         writePropFields(iwriter, buildable)
-        iwriter.endBlock(1)
 
+        iwriter.skip()
+        writeConstructor(iwriter, buildable)
+
+        iwriter.skip()
+        writeBuilderClass(iwriter, buildable)
+
+        iwriter.endBlock(1)
+        iwriter.writeLine("}")
     }
 }
 
