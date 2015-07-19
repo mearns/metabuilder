74005bc15853413258ebe190dc5dbc868e16cdc4
Brian Mearns <bmearns@ieee.org>
2015-07-18 20:39:08 -0400

    Working on an indented writer.

--- src/main/groovy/org/ieee/bmearns/metabuilder/generators/util/IndentedWriter.groovy
+++ src/main/groovy/org/ieee/bmearns/metabuilder/generators/util/IndentedWriter.groovy
@@ -7,13 +7,17 @@ class IndentedWriter {
     private int indentLevel;
     private String tabString;
     private String indent;
+    private Stack<Integer> blockStack;
 
     IndentedWriter(Writer writer, String tabString, int indentLevel) {
+        this.blockStack = new Stack<>();
         this.writer = writer
         this.tabString = tabString
         this.indentLevel = indentLevel
         this.updateIndent()
     }
+
+    public static class BlockException extends RuntimeException {}
 
     protected void updateIndent() {
         StringBuilder sb = new StringBuilder(indentLevel * tabString.length())
@@ -37,8 +41,18 @@ class IndentedWriter {
         line.each{ writeLine(it) }
     }
 
-    public IndentedWriter writeText(String text) {
-        text.split(/\n/).each{ writeLine(it) }
+    public IndentedWriter block(int indent) {
+        blockStack.push(this.indentLevel)
+        this.indent(indent)
+    }
+
+    public IndentedWriter endBlock(int outdent) 
+        throws IndexOutOfBoundsException, BlockException {
+        this.outdent(outdent)
+        if (blockStack.empty()) {
+            throw new IndexOutOfBoundsException("Attempted to close a block, but no blocks currently open.")
+        }
+        int expectedLevel = blockStack.pop()
     }
 
     public IndentedWriter indent() {
